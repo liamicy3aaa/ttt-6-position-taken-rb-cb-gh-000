2b7346d36ef1effa8c79af4f6a9e74acf6d3db01
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 14:33:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  bitem = board[index]
+  if bitem == "" || bitem == " " || bitem == nil
+    return false
+  else
+    return true
+  end
+end
