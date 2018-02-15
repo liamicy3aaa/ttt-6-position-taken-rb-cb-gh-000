38b4583c445116f00fc40ef9da6af761da6752d0
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 14:29:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  item =
+  nindex = index.to_i - 1 
+  bitem = board[nidex]
+  if(bitem == "" || bitem == " " || bitem == nil)
+    return false
+  end
+end
